05c4db7951bde3e2abafc5d3ca0f8f0bc2a0840d
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-23 16:20:26 -0700

    this is the answer of total-groceries

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// Good Luck 💪🏾
+function getTotalPrice(groceries) {
+    const prices = groceries.map(item => item.quantity * item.price);
+    const total = prices.reduce((acc, curr) => acc + curr, 0);
+    return total;
+  }
+  const groceries1 = [
+    { product: "Milk", quantity: 1, price: 1.50 }
+  ];
+  
+  console.log(getTotalPrice(groceries1)); 
+  
+  const groceries2 = [
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Cereals", quantity: 1, price: 2.50 },
+  ];
+  
+  console.log(getTotalPrice(groceries2)); 
+  
+  const groceries3 = [
+    { product: "Milk", quantity: 3, price: 1.50 }
+  ];
+  
+  console.log(getTotalPrice(groceries3)); 
